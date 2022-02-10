81f3dee851be0ed1e69ae9f7119b58b352afaa28
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-02-10 08:54:16 +0300

    inital commit

---
+++ src/components/modules/UserProfile/BioBox/BioBox.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+import { Wrapper, Bio } from "./StyledBioBox";
+
+import { env } from "../../../../utils/EnvironmentVariables";
+
+export default function BioBox({ data }) {
+  const [bioText, setBioText] = useState(data.bio);
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const handleTextChange = (e) => {
+    setBioText(e.target.value);
+  };
+
+  const sendText = () => {
+    axios
+      .put(
+        `${env().STRAPI_URL}/users/${data.id}/profiles`,
+        { bio: bioText },
+        { withCredentials: true }
+      )
+      .then(() => {
+        // TODO give user feedback that it worked
+        setIsReadOnly(true);
+      })
+      .catch(() => {
+        // TODO handle errors
+      });
+  };
+  return (
+    <Wrapper>
+      <br />
+      <Bio
+        rows="2"
+        cols="50"
+        placeholder="Write your bio here!"
+        maxlength="144"
+        value={bioText}
+        onDoubleClick={() => setIsReadOnly(false)}
+        onChange={handleTextChange}
+        readOnly={isReadOnly}
+      ></Bio>
+      <br />
+      {isReadOnly ? (
+        <button onClick={() => setIsReadOnly(false)}>Edit</button>
+      ) : (
+        <button onClick={(() => setIsReadOnly(true), sendText)}>Save</button>
+      )}
+    </Wrapper>
+  );
+}
---
+++ src/components/modules/UserProfile/UserProfile.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+
+import PageBody from "../../common/PageBody";
+import Button from "../../common/Button";
+
+import ProfileCard from "./ProfileCard";
+import Points from "./Points";
+import BioBox from "./BioBox";
+import WeeksGlance from "./WeeksGlance";
+import { useUserDataContext } from "../../../context/UserDataContext";
+
+import { env } from "../../../utils/EnvironmentVariables";
+
+import { Wrapper, UserSection, UserInfo, Misc } from "./StyledUserProfile";
+// import DiscordSection from "./DiscordSection/DiscordSection";
+
+export default function UserProfile() {
+  const { userData } = useUserDataContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(userData.id === -1);
+  }, [userData]);
+
+  if (loading) {
+    return <strong>Loading.....</strong>;
+  }
+
+  return (
+    <PageBody>
+      {userData.id && !loading ? (
+        <Wrapper>
+          <UserSection>
+            <ProfileCard
+              img={userData.profilePictureUrl}
+              name={userData.name}
+              username={userData.username}
+            />
+            <UserInfo>
+              <Points
+                availablePoints={userData.availablePoints}
+                seasonPoints={userData.totalSeasonPoints}
+                volunteerHours={userData.volunteerHours}
+              />
+              <BioBox data={userData}>{userData.bio}</BioBox>
+            </UserInfo>
+          </UserSection>
+          {/*
+          <LabCampus />
+          */}
+          <WeeksGlance />
+
+          <Misc>
+            {/*
+            <LabMember />
+            */}
+            {/*
+              <DiscordSection
+                discordId={userData.discord.id}
+                avatarKey={userData.discord.avatar}
+                discordUsername={userData.discord.username}
+                discordDiscriminator={userData.discord.discriminator}
+              /> */}
+          </Misc>
+        </Wrapper>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "60vh",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p style={{ fontSize: "2rem" }}>
+            Please sign in to access this page!
+          </p>
+          <Button fontSize="2rem" href={env().GOOGLE_AUTH_URL}>
+            Sign In
+          </Button>
+          <br />
+        </div>
+      )}
+      <br />
+    </PageBody>
+  );
+}
